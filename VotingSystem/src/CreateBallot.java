1efc910796402360309be939bfed8dfeb623871a
nteghanemt <nadim-teghanemt@uiowa.edu>
2020-12-05 16:14:40 -0600

    can now add to database

--- VotingSystem/src/CreateBallot.java
+++ VotingSystem/src/CreateBallot.java
@@ -2,19 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class CreateBallot extends JFrame {
     CreateBallot(String county, String state){
         super("Create Ballot for "+county+", "+ state);
         setLayout(new FlowLayout());
 
-
-        JTextField jtf1 = new JTextField("Enter title of position");
-        JTextField jtf2 = new JTextField("Enter name of candidate");
+        JTextField jtfForPosition = new JTextField("Enter title of position");
+        JTextField jtfForName = new JTextField("Enter name of candidate");
         JButton jb1 = new JButton("Submit to database");
         JButton jb2 = new JButton("Remove from database");
 
@@ -27,8 +24,34 @@ public class CreateBallot extends JFrame {
                     Connection connection = DriverManager.getConnection("jdbc:mysql://s-l112.engr.uiowa.edu:3306/engr_class011", "engr_class011", "dbforece!");
                     Statement statement = connection.createStatement();
 
+                    //Get list of positions in the registry
+                    ResultSet rs = statement.executeQuery("SELECT Choice FROM Ballot");
+                    ArrayList allChoices = new ArrayList<String>();
+                    while (rs.next()) {
+                        allChoices.add(rs.getString(1));
+                    }
+                    boolean exists = false;
+                    //Check if this license is already in the database
+                    for (Object name : allChoices) {
+                        if (jtfForName.getText().equals(name)) {
+                            exists = true;
+                        }
+                    }
 
-
+                    if (!exists){
+                        PreparedStatement insert = connection.prepareStatement("INSERT INTO Ballot " + "(Choice,County,State,Position,Votes) " + "VALUES (?,?,?,?,?)");
+                        insert.setString(1,jtfForName.getText());
+                        insert.setString(2,county);
+                        insert.setString(3,state);
+                        insert.setString(4,jtfForPosition.getText());
+                        insert.setInt(5,0);
+                        insert.executeUpdate();
+                    }
+                    else{
+                        JPopupMenu temp = new JPopupMenu("Error");
+                        temp.add(new JLabel("This person is already a candidate"));
+                        temp.setVisible(true);
+                    }
                 }
                 catch(SQLException e){
                     e.printStackTrace();
@@ -36,6 +59,7 @@ public class CreateBallot extends JFrame {
             }
         });
 
+        //REMOVE FROM DATABASE
         jb2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -45,7 +69,6 @@ public class CreateBallot extends JFrame {
                     Statement statement = connection.createStatement();
 
 
-
                 }
                 catch(SQLException e){
                     e.printStackTrace();
@@ -53,9 +76,9 @@ public class CreateBallot extends JFrame {
             }
         });
 
-        add(jtf1);
+        add(jtfForName);
+        add(jtfForPosition);
         add(jb1);
-        add(jtf2);
         add(jb2);
 
 
