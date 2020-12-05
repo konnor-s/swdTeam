2f19e864e3197261abfcd0974f9cc8700f6391f8
kksommer <kksommer@uiowa.edu>
2020-12-04 20:29:48 -0600

    Some ideas for the ballot

--- VotingSystem/src/Ballot.java
+++ VotingSystem/src/Ballot.java
@@ -1,7 +1,38 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class Ballot extends JFrame {
     Ballot(String country, String state){
         super("Voter Ballot");
+
+        int p =9 //should be number of positions from database.
+
+        setLayout(new GridLayout(1,2));
+        JPanel lPanel = new JPanel();
+        JPanel rPanel = new JPanel();
+        lPanel.setLayout(new FlowLayout());
+        rPanel.setLayout(new FlowLayout());
+
+        ArrayList positions = new ArrayList<ButtonGroup>();
+        //for(String votePositions in database positions category){
+
+
+            //create new JPanel in either left or right column
+            if(i<=Math.ceil(p/2))){
+                lPanel.add(new JPanel())
+}
+            else{
+                rPanel.add(new JPanel());
+             }
+            //create button group
+            //
+
+            //for(int l = 0; l<p;l++){
+                //create button for this candidate.
+                //add button to button group.
+                //add button to panel
+            //add panel to frame
+
     }
 }
