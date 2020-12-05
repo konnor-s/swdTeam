dca65ee02c39c70ee25c1a4249bd564e0f20825c
kksommer <kksommer@uiowa.edu>
2020-12-04 19:40:34 -0600

    Creating classes

---
+++ VotingSystem/src/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+    public static void main(String args[]){
+        PrimaryInterface gui = new PrimaryInterface();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(1200,800);
+        gui.setVisible(true);
+
+    }
+}
